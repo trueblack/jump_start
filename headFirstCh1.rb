409affcc187bd5bee4b23a5e294641ec5ab6004b
trueblack <trueblack@zoho.com>
2020-04-27 12:38:34 -0700

    another test commit

--- headFirstCh1.rb
+++ headFirstCh1.rb
@@ -1,5 +1,6 @@
 # prompt player to enter name
-
+print "Hello User! Let's play a game! What is your name? "
+userName = gets.chomp
 # generate random number 1-100 and store for player to guess
 
 # keep track of how many guesses, each guess: prompt w remaining #
