ed95e18ef9e61885eb5211fcadc60089ee52f533
sara <hunkspouse@gmail.com>
2020-04-27 11:26:10 -0800

    Update headFirstCh1.rb
    housekeeping to ensure attribution in commit history

--- headFirstCh1.rb
+++ headFirstCh1.rb
@@ -1,6 +1,11 @@
 # prompt player to enter name
+
 # generate random number 1-100 and store for player to guess
+
 # keep track of how many guesses, each guess: prompt w remaining #
+
 # give player feedback higher or lower
+
 # if players guess is correct, let them know!
+
 # if player runs out of turns, let them know
